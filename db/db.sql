f5fcbf670378939774a5763afa628c5bdbc86989
Erika <88861951+ErikaRoguie@users.noreply.github.com>
2022-03-11 17:50:41 -0700

    add Routers

--- db/db.sql
+++ db/db.sql
@@ -1,17 +1,3 @@
-CREATE TABLE parties (
-  id INTEGER AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(50) NOT NULL,
-  description TEXT
-);
-INSERT INTO candidates (first_name, last_name, industry_connected)
-VALUES
-  ('Ronald', 'Firbank', 1),
-  ('Virginia', 'Woolf', 1),
-  ('Piers', 'Gaveston', 0),
-  ('Charles', 'LeRoi', 1),
-  ('Katherine', 'Mansfield', 1),
-  ('Dora', 'Carrington', 0),
-  ('Edward', 'Bellamy', 0),
-  ('Montague', 'Summers', 1),
-  ('Octavia', 'Butler', 1),
-  ('Unica', 'Zurn', 1);
+DROP DATABASE IF EXISTS election;
+CREATE DATABASE election;
+USE election;
