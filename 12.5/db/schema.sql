f354068425d07d3421de8f23181654078e15972d
Erika <88861951+ErikaRoguie@users.noreply.github.com>
2022-03-12 14:29:53 -0700

    finished(?)

--- 12.5/db/schema.sql
+++ 12.5/db/schema.sql
@@ -15,7 +15,10 @@ CREATE TABLE candidates (
   last_name VARCHAR(30) NOT NULL,
   party_id INTEGER,
   industry_connected BOOLEAN NOT NULL,
-  CONSTRAINT fk_party FOREIGN KEY (party_id) REFERENCES parties(id) ON DELETE SET NULL
+  CONSTRAINT fk_party
+    FOREIGN KEY (party_id)
+    REFERENCES parties(id)
+    ON DELETE SET NULL
 );
 
 CREATE TABLE voters (
@@ -31,4 +34,7 @@ CREATE TABLE votes (
   voter_id INTEGER NOT NULL,
   candidate_id INTEGER NOT NULL,
   created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  CONSTRAINT uc_voter UNIQUE (voter_id),
+  CONSTRAINT fk_voter FOREIGN KEY (voter_id) REFERENCES voters(id) ON DELETE CASCADE,
+  CONSTRAINT fk_candidate FOREIGN KEY (candidate_id) REFERENCES candidates(id) ON DELETE CASCADE
 );
